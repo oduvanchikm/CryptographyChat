d046075b92c080302483410add12f161644282e5
oduvanchikm <mkgubareva2005@gmail.com>
2025-05-18 17:07:27 +0400

    final changes

--- Cryptography/ContextCrypto.cs
+++ Cryptography/ContextCrypto.cs
@@ -120,10 +120,6 @@ public class ContextCrypto
             CipherMode.CipherMode.Mode.NONE => _encryptor.Decrypt(data),
             _ => throw new NotSupportedException()
         };
-        // if (_cipherMode != CipherMode.CipherMode.Mode.NONE)
-        // {
-        //     return PaddingMode.PaddingMode.DeletePadding(decryptedData, _paddingMode);
-        // }
 
         return decryptedData;
     }
